3afc5fce5d3d30ab8572b997cad767a39add91bc
M-Abhijit07 <b210039@nitsikkim.ac.in>
2024-06-12 20:28:33 +0530

    changed to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    theme: {
+        extend: {
+            animation: {
+                "loop-scroll": "loop-scroll 50s linear infinite"
+            },
+            keyframes: {
+                "loop-scroll": {
+                    from: { transform : "translateX(0)" },
+                    to: { transform : "translateX(-100%)" },
+                },
+            },
+        },
+    },
+    plugins: [],
+};
